cd64e20a0453c77b7e385b49adce13390a56119a
15chrjef <jchristian50@gmail.com>
2020-08-15 15:27:53 -0400

    fix buttons for real

--- components/BlogGenerator.tsx
+++ components/BlogGenerator.tsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState} from "react";
 import TitleWithTooltip from './TitleWithTooltip'
 import Button from '@material-ui/core/Button';
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
-const ColorButton = withStyles(() => ({
-  root: {
-    // color: theme.palette.getContrastText(purple[500]),
-		backgroundColor: '#CDFFDB',
-		border: '1px solid #797979',
-		borderRadius:'20px',
-		fontSize: '36px',
-		padding: '0px',
-		marginLeft: '10px',
-		boxShadow: 'none',
-		'&:hover': {
-			backgroundColor: "#FFD5D5",
-			boxShadow: 'none',
-    },
-  },
-}))(Button);
-
 const BlogGenerator: React.FC = () => {
+	const [likeButtonsState, setLikeButtonsState] = useState({
+		like: {
+			selected: false,
+			hovered: false
+		},
+		dislike: {
+			selected: false,
+			hovered: false
+		}
+	})
+	const { like, dislike } = likeButtonsState
+	const likeButtonStyles = like.selected || like.hovered ? {...unselectedDraftStyles, ...selectedDraftStyles} : {...unselectedDraftStyles}
+	const dislikeButtonStyles = dislike.selected || dislike.hovered ? {...unselectedDraftStyles, ...selectedDraftStyles} : {...unselectedDraftStyles}
+
+	const handleLikeButtonClick = (type) => {
+		const typeMap = { like: 'dislike', dislike: 'like'}
+		let newState = {...likeButtonsState}
+		newState[type].selected = true
+		newState[typeMap[type]].selected = false
+		setLikeButtonsState(newState)
+	}
+
+	const handleLikeButtonMouseIn = (type) => {
+		let newState = {...likeButtonsState}
+		newState[type].hovered = true
+		setLikeButtonsState(newState)
+	}
+
+	const handleLikeButtonMouseOut = (type) => {
+		let newState = {...likeButtonsState}
+		newState[type].hovered = false
+		setLikeButtonsState(newState)
+	}
+
   return (
 		<div style={{padding: '0 5vw 0 5vw'}}>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -51,16 +68,42 @@ const BlogGenerator: React.FC = () => {
         </div>
         <div style={{display: 'flex', alignItems: 'center'}}>
           <div>Will you use this draft?</div>
-					<ColorButton variant="contained" color="primary">
+					<Button 
+						onClick={() => handleLikeButtonClick('like')}
+						onMouseEnter={() => handleLikeButtonMouseIn('like')}
+						onMouseLeave={() => handleLikeButtonMouseOut('like')}
+						style={likeButtonStyles} 
+						disableElevation variant="contained" color="primary"
+					>
 						👍
-					</ColorButton>
-					<ColorButton variant="contained" color="primary">
+					</Button>
+					<Button 
+						onClick={() => handleLikeButtonClick('dislike')}
+						onMouseEnter={() => handleLikeButtonMouseIn('dislike')}
+						onMouseLeave={() => handleLikeButtonMouseOut('dislike')}
+						style={dislikeButtonStyles} 
+						disableElevation variant="contained" color="primary"
+					>
 						👎
-					</ColorButton>
+					</Button>
         </div>
       </div>
     </div>
   );
 };
+
+const unselectedDraftStyles = {
+	backgroundColor: '#CDFFDB',
+	border: '1px solid #797979',
+	borderRadius:'20px',
+	fontSize: '36px',
+	padding: '0px',
+	marginLeft: '10px',
+	boxShadow: 'none',
+}
+
+const selectedDraftStyles = {
+	backgroundColor: '#FFD5D5',
+}
 
 export default BlogGenerator;
