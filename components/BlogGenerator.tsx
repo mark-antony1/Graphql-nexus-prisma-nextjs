e968186080c96f43895c3afc462f04a04eed2264
15chrjef <jchristian50@gmail.com>
2020-08-15 16:27:08 -0400

    add loading button + blog generation call

--- components/BlogGenerator.tsx
+++ components/BlogGenerator.tsx
@@ -2,13 +2,24 @@ import React, { useState} from "react";
 import TitleWithTooltip from './TitleWithTooltip'
 import Button from '@material-ui/core/Button';
 import BlogLikeButtons from './BlogLikeButtons'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios'
 
 const BlogGenerator: React.FC = () => {
 	const [blogTitleText, setBlogTitleText] = useState("")
 	const [exampleBlogTitleText, setExampleBlogTitleText] = useState("")
 	const [exampleBlogText, setExampleBlogText] = useState("")
 	const [generatedBlogText, setGeneratedBlogText] = useState("")
+	const [isLoadingBlog, setIsLoadingBlog] = useState(false)
+	
 
+	const generateBlog = () => {
+		setIsLoadingBlog(true)
+		setTimeout(() => {
+			setGeneratedBlogText("Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum")
+			setIsLoadingBlog(false)
+		}, 3000)
+	}
   return (
 		<div style={{padding: '0 5vw 0 5vw'}}>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -47,9 +58,9 @@ const BlogGenerator: React.FC = () => {
       </div>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
         <div>
-          <div>712/900</div>
-					<Button disableElevation variant="contained" color="primary">
-						Generate
+          <div>{Number(exampleBlogText.length) + Number(exampleBlogTitleText.length)}/900</div>
+					<Button style={{minWidth: '120px', minHeight: "39px"}} onClick={generateBlog} disableElevation variant="contained" color="primary">
+						{isLoadingBlog ? <CircularProgress color="white" size='26px'/> :"Generate"}
 					</Button>
         </div>
 				<BlogLikeButtons/>
