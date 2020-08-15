c51c5fa6b10d4cfdcd1c13cc44b092955cdb8d5c
15chrjef <jchristian50@gmail.com>
2020-08-15 15:55:38 -0400

    wire up basic text area state

--- components/BlogGenerator.tsx
+++ components/BlogGenerator.tsx
@@ -4,25 +4,45 @@ import Button from '@material-ui/core/Button';
 import BlogLikeButtons from './BlogLikeButtons'
 
 const BlogGenerator: React.FC = () => {
+	const [blogTitleText, setBlogTitleText] = useState("")
+	const [exampleBlogTitleText, setExampleBlogTitleText] = useState("")
+	const [exampleBlogText, setExampleBlogText] = useState("")
+	const [generatedBlogText, setGeneratedBlogText] = useState("")
+
   return (
 		<div style={{padding: '0 5vw 0 5vw'}}>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
         <div>
 					<TitleWithTooltip titleText='New Blog Title' tooltipText='Input the title for the blog post you want to create'/>
-          <input></input>
+					<input 
+						value={blogTitleText} 
+						onChange={(e) => setBlogTitleText(e.target.value)}
+					/>
 					<TitleWithTooltip titleText='Blog Example' tooltipText='Input the title for the sample blog post'/>
           <div>Blog Example Title</div>
-          <input></input>
+					<input 
+						value={exampleBlogTitleText} 
+						onChange={(e) => setExampleBlogTitleText(e.target.value)}
+					/>
           <div>Blog Example Text</div>
 					<div className='pro-tip' style={{background: "#9ED1A9", borderRadius: '3px', width: "35vw"}}>
 						<b style={{fontWeight: 'bold', color: 'blue'}}>PRO TIP</b>
 						: For best results, include raw text only. Clean up alt text from pasted images, those huge blocks of whitespace, and any weird residual text from links. Leave the title out.
 					</div>
-          <textarea style={{width: '40vw', height: '40vh'}}></textarea>
+					<textarea 
+						value={exampleBlogText} 
+						onChange={(e) => setExampleBlogText(e.target.value)}
+						style={{width: '40vw', height: '40vh'}}
+					/>
         </div>
         <div>
 					<TitleWithTooltip titleText='Blog Output' tooltipText='this is the generated text for the blog you are creating'/>
-          <textarea style={{width: '45vw', height: '60vh'}}></textarea>
+					<textarea 
+						disabled
+						value={generatedBlogText} 
+						onChange={(e) => setGeneratedBlogText(e.target.value)} 
+						style={{width: '45vw', height: '60vh'}}
+					/>
         </div>
       </div>
       <div style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -37,19 +57,5 @@ const BlogGenerator: React.FC = () => {
     </div>
   );
 };
-
-const unselectedDraftStyles = {
-	backgroundColor: '#CDFFDB',
-	border: '1px solid #797979',
-	borderRadius:'20px',
-	fontSize: '36px',
-	padding: '0px',
-	marginLeft: '10px',
-	boxShadow: 'none',
-}
-
-const selectedDraftStyles = {
-	backgroundColor: '#FFD5D5',
-}
 
 export default BlogGenerator;
